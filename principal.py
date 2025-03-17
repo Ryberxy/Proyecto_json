156bc5cccc6145adadc5285824569f3168f253d5
Ryberxy <gagomartin2006@gmail.com>
2025-03-17 15:04:58 +0100

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,1 +1,37 @@
+import json 
+import time
+import sys
+from funciones import *
 
+with open('futbol.json', 'r') as archivo:
+    datos = json.load(archivo)
+
+equipos = datos['equipos']
+
+menu = '''
+1.Listar equipos
+2.Mostrar equipos y jugadores
+3.Filtrar jugadores por su posición
+4.Buscar información de un equipo y su capitán
+5.Buscar jugadores por dorsal
+6.Salir del programa
+Ingrese una opción: '''
+
+opcion = -1
+
+while opcion != 0:
+    opcion = int(input(menu))
+    if opcion == 1:
+        listar_equipos(equipos)
+    elif opcion == 2:
+        mostrar_jugadores(equipos)
+    elif opcion == 3:
+        filtrar_posicion(equipos)
+    elif opcion == 4:
+        capitan(equipos)
+    elif opcion == 5:
+        buscar_dorsal(equipos)
+    elif opcion == 6:
+        salir()
+    else:
+        print("Por favor, introduzca un número correcto(1-6): ")
